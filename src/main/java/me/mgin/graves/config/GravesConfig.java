20e141a63eb5c1ec9b0873431f070002f912533d
ginsm <mattg.0609@gmail.com>
2021-12-02 15:41:15 -0800

    Add post load validation; exclude operatorOverrideLevel
    Operator Override Level is very much a server configuration option. It
    doesn't belong in the GUI like the rest of the options IMO.

--- src/main/java/me/mgin/graves/config/GravesConfig.java
+++ src/main/java/me/mgin/graves/config/GravesConfig.java
@@ -5,13 +5,21 @@ import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
-@Config(name = "graves")
+@Config(name = "forgottengraves")
 public class GravesConfig implements ConfigData {
   @ConfigEntry.Gui.CollapsibleObject(startExpanded = true)
   public MainSettings mainSettings = new MainSettings();
 
   public static GravesConfig getConfig() {
     return AutoConfig.getConfigHolder(GravesConfig.class).getConfig();
+  }
+
+  @Override
+  public void validatePostLoad() {
+    mainSettings.customXPStoredLevel = Math.max(mainSettings.customXPStoredLevel, 0);
+    mainSettings.minOperatorOverrideLevel = Math.max(
+      Math.min(mainSettings.minOperatorOverrideLevel, 4), -1
+    );
   }
 
   public static class MainSettings {
@@ -23,7 +31,6 @@ public class GravesConfig implements ConfigData {
 
     @ConfigEntry.Gui.Tooltip
     public boolean sendGraveCoordinates = true;
-
     
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
@@ -40,7 +47,7 @@ public class GravesConfig implements ConfigData {
     @ConfigEntry.Gui.Tooltip
     public int customXPStoredLevel = 30;
     
-    @ConfigEntry.Gui.Tooltip
-    public int operatorOverrideLevel = 4;
+    @ConfigEntry.Gui.Excluded
+    public int minOperatorOverrideLevel = 4;
   }
 }

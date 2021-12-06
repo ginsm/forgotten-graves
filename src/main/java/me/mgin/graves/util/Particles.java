5d35d34c961a0212993c2b06c93d0c7184f03da1
ginsm <mattg.0609@gmail.com>
2021-12-06 02:18:34 -0800

    Add spawnAtBlockBottom method
    Responsible for spawning particles at the bottom of the block.

--- src/main/java/me/mgin/graves/util/Particles.java
+++ src/main/java/me/mgin/graves/util/Particles.java
@@ -7,10 +7,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class Particles {
-	static public void spawnAtBlock(DefaultParticleType type, int amount, int maxVelocity, World world,
-			BlockPos blockPos) {
-		Random rng = new Random();
+	private static Random rng = new Random();
 
+	static public void spawnAtBlock(World world, BlockPos blockPos, DefaultParticleType type, int amount,
+			double maxVelocity) {
 		for (int i = 0; i <= amount; i++) {
 			double blockX = blockPos.getX() + rng.nextDouble();
 			double blockY = blockPos.getY() + rng.nextDouble();
@@ -22,6 +22,30 @@ public class Particles {
 
 			world.addParticle(type, blockX, blockY, blockZ, velocityX, velocityY, velocityZ);
 		}
+	}
 
+	/**
+	 * Spawn particles at the bottom of a block
+	 *
+	 * @param world
+	 * @param blockPos
+	 * @param type
+	 * @param amount
+	 * @param maxVelocity
+	 * @param maxStartHeight
+	 */
+	static public void spawnAtBlockBottom(World world, BlockPos blockPos, DefaultParticleType type, int amount,
+			double maxVelocity, double maxStartHeight) {
+		for (int i = 0; i <= amount; i++) {
+			double blockX = blockPos.getX() + rng.nextDouble();
+			double blockY = Math.min(blockPos.getY() + rng.nextDouble(), blockPos.getY() + maxStartHeight);
+			double blockZ = blockPos.getZ() + rng.nextDouble();
+
+			double velocityX = rng.nextDouble() * maxVelocity;
+			double velocityY = rng.nextDouble() * maxVelocity;
+			double velocityZ = rng.nextDouble() * maxVelocity;
+
+			world.addParticle(type, blockX, blockY, blockZ, velocityX, velocityY, velocityZ);
+		}
 	}
 }

7c5d6a310f7de1b15189091151a50c0e63d1a5e5
ginsm <mattg.0609@gmail.com>
2022-01-10 20:10:09 -0800

    Do not redirect handling when enableGraves false

--- src/main/java/me/mgin/graves/events/TrinketDropHandler.java
+++ src/main/java/me/mgin/graves/events/TrinketDropHandler.java
@@ -2,12 +2,17 @@ package me.mgin.graves.events;
 
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketEnums.DropRule;
+import me.mgin.graves.config.GravesConfig;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 
 public class TrinketDropHandler {
   public static DropRule handleTrinketDrop(DropRule rule, ItemStack stack, SlotReference ref, LivingEntity entity) {
     // Prevent Trinkets from handling dropInventory
-    return DropRule.KEEP;
+    if (GravesConfig.getConfig().mainSettings.enableGraves) {
+      return DropRule.KEEP;
+    }
+
+    return rule;
   }
 }

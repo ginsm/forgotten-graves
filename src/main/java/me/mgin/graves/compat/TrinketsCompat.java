337fb5fdd59015cb3c1ce1b426cf71349ff5dfdb
ginsm <mattg.0609@gmail.com>
2022-01-12 17:30:53 -0800

    Rework (get|set)Inventory & add equipItem
    - getInventory now gets every slot -- any slot that is empty simply
      gets stored as air.
    - setInventory now uses TrinketCompat.equipItem
    - equipItem expects an ItemStack, PlayerEntity, and int (index);
      it will use that index to equip the ItemStack on the specified
      PlayerEntity.

--- src/main/java/me/mgin/graves/compat/TrinketsCompat.java
+++ src/main/java/me/mgin/graves/compat/TrinketsCompat.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import dev.emi.trinkets.TrinketSlot;
+import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketInventory;
 import dev.emi.trinkets.api.TrinketItem;
@@ -11,10 +13,13 @@ import dev.emi.trinkets.api.TrinketsApi;
 import me.mgin.graves.api.GravesApi;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.world.event.GameEvent;
 
 public class TrinketsCompat implements GravesApi {
 	/**
 	 * Retrieve a list containing items occupying the trinket slots.
+	 *
 	 * @param player
 	 * @return List<ItemStack>
 	 */
@@ -24,8 +29,7 @@ public class TrinketsCompat implements GravesApi {
 		List<ItemStack> itemStacks = new ArrayList<>();
 
 		if (component.isPresent()) {
-			component.get().getAllEquipped().forEach((pair) -> {
-				ItemStack itemStack = pair.getRight();
+			component.get().forEach((ref, itemStack) -> {
 				itemStacks.add(itemStack);
 			});
 		}
@@ -35,18 +39,21 @@ public class TrinketsCompat implements GravesApi {
 
 	/**
 	 * Equips all items within a list of ItemStacks into the trinket slots.
-	 * @param inventory - List<ItemStack>
+	 *
+	 * @param inventory
+	 *            - List<ItemStack>
 	 * @param player
 	 */
 	@Override
 	public void setInventory(List<ItemStack> inventory, PlayerEntity player) {
-		for (ItemStack itemStack : inventory) {
-			TrinketItem.equipItem(player, itemStack);
+		for (int i = 0; i < inventory.size(); i++) {
+			equipItem(inventory.get(i), player, i);
 		}
 	}
 
 	/**
 	 * Retrieve the amount of trinket slots available.
+	 *
 	 * @param player
 	 * @return int
 	 */
@@ -67,16 +74,53 @@ public class TrinketsCompat implements GravesApi {
 
 	/**
 	 * Remove all items from the trinket slots.
+	 *
 	 * @param player
 	 */
 	public static void clearInventory(PlayerEntity player) {
 		Optional<TrinketComponent> component = TrinketsApi.getTrinketComponent(player);
 
 		if (component.isPresent()) {
-			component.get().forEach((ref, stack) -> {
+			component.get().forEach((ref, itemStack) -> {
 				TrinketInventory inventory = ref.inventory();
 				inventory.setStack(ref.index(), ItemStack.EMPTY);
 			});
+		}
+	}
+
+	/**
+	 * Equips an item based on a given index; this is meant to be used with
+	 * setInventory. The index is based on each TrinketSlot -- not group.
+	 * 
+	 * @param item
+	 * @param player
+	 * @param index
+	 */
+	public static void equipItem(ItemStack item, PlayerEntity player, int index) {
+		Optional<TrinketComponent> component = TrinketsApi.getTrinketComponent(player);
+
+		if (component.isPresent()) {
+			int currentSlot = 0;
+			for (var group : component.get().getInventory().values()) {
+				for (TrinketInventory inventory : group.values()) {
+					for (int i = 0; i < inventory.size(); i++) {
+						if (currentSlot == index) {
+							SlotReference ref = new SlotReference(inventory, index);
+							if (TrinketSlot.canInsert(item, ref, player)) {
+								ItemStack newStack = item.copy();
+								inventory.setStack(i, newStack);
+								SoundEvent soundEvent = item.getEquipSound();
+								if (!item.isEmpty() && soundEvent != null) {
+									 player.emitGameEvent(GameEvent.EQUIP);
+									 player.playSound(soundEvent, 1.0F, 1.0F);
+								}
+								item.setCount(0);
+							}
+						}
+						currentSlot += 1;
+					}
+				}
+			}
 		}
 	}
 }

ce8458f6a425a16f77e04629e33b0f16b1b74e92
ginsm <mattg.0609@gmail.com>
2022-01-17 20:51:11 -0800

    Return unequipped items from setInventory

--- src/main/java/me/mgin/graves/api/GravesApi.java
+++ src/main/java/me/mgin/graves/api/GravesApi.java
@@ -3,11 +3,19 @@ package me.mgin.graves.api;
 import java.util.List;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 
 public interface GravesApi {
 	List<ItemStack> getInventory(PlayerEntity entity);
 
-	void setInventory(List<ItemStack> inventory, PlayerEntity entity);
+	/**
+	 * Set the inventory for a given mod.
+	 * 
+	 * @param inventory
+	 * @param entity
+	 * @return Items that could not be equipped.
+	 */
+	DefaultedList<ItemStack> setInventory(List<ItemStack> inventory, PlayerEntity entity);
 
 	int getInventorySize(PlayerEntity entity);
 }

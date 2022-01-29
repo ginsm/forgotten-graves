cfefd5dcf9fdc4ddb40aaa6b46dd5b33e28519ea
ginsm <mattg.0609@gmail.com>
2022-01-28 20:14:27 -0800

    Create dedicated commands directory

---
+++ src/main/java/me/mgin/graves/commands/ReloadCommand.java
@@ -0,0 +1,21 @@
+package me.mgin.graves.commands;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
+
+import me.mgin.graves.config.GravesConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+
+public class ReloadCommand {
+  static public int execute(CommandContext<FabricClientCommandSource> context) {
+    AutoConfig.getConfigHolder(GravesConfig.class).load();
+    context.getSource().sendFeedback(
+      new TranslatableText("text.forgottengraves.command.reload")
+        .formatted(Formatting.GRAY)
+    );
+    return Command.SINGLE_SUCCESS;
+  }
+}
--- src/main/java/me/mgin/graves/registry/RegisterCommands.java
+++ src/main/java/me/mgin/graves/registry/RegisterCommands.java
@@ -1,15 +1,10 @@
 package me.mgin.graves.registry;
 
 import static net.minecraft.server.command.CommandManager.literal;
-import com.mojang.brigadier.Command;
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.context.CommandContext;
-import me.mgin.graves.config.GravesConfig;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.mgin.graves.commands.GravesCommand;
+import me.mgin.graves.commands.ReloadServerCommand;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Formatting;
+
 
 public class RegisterCommands {
 
@@ -17,53 +12,16 @@ public class RegisterCommands {
    * Registers all of the commands associated with Forgotten Graves.
    */
   public static void register() {
+    // Register server-side commands
     CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) ->
-      registerGravesCommands(dispatcher)
+      dispatcher.register(
+        literal("graves")
+          .executes(context -> GravesCommand.execute(context))
+          .then(literal("reload")
+            .requires(source -> source.hasPermissionLevel(4))
+            .executes(context -> ReloadServerCommand.execute(context)))
+      )
     );
-  }
-
-  /**
-   * Currently registers 1 command:
-   * reload - Reloads the AutoConfig config.
-   * 
-   * Planned commands:
-   * lookup - Provides a list of all graves (unless a player is selected)
-   * config - 
-   * @param dispatcher
-   */
-  private static void registerGravesCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
-    dispatcher.register(
-      literal("graves")
-        .executes(context -> Commands.graves(context))
-        .then(literal("reload")
-          .requires(source -> source.hasPermissionLevel(4))
-          .executes(context -> Commands.reload(context)))
-    );
-  }
-
-
-  /**
-   * This class contains various command handler methods.
-   */
-  private static class Commands {
-
-    static final int graves(CommandContext<ServerCommandSource> context) {
-      context.getSource().sendError(
-        new TranslatableText("text.forgottengraves.command.graves")
-      );
-      return Command.SINGLE_SUCCESS;
-    }
-    
-    static final int reload(CommandContext<ServerCommandSource> context) {
-      AutoConfig.getConfigHolder(GravesConfig.class).load();
-      context.getSource().sendFeedback(
-        new TranslatableText("text.forgottengraves.command.reload")
-          .formatted(Formatting.GRAY),
-        true
-      );
-      return Command.SINGLE_SUCCESS;
-    }
-
   }
 
 }

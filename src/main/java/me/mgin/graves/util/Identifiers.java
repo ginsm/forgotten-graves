928d4db796c0ca67be8ef92854b27a390dbc1623
ginsm <mattg.0609@gmail.com>
2022-02-01 04:12:52 -0800

    Fix invalid CLIENT_SEND_CONFIG identifier

--- src/main/java/me/mgin/graves/util/Identifiers.java
+++ src/main/java/me/mgin/graves/util/Identifiers.java
@@ -1,7 +1,8 @@
 package me.mgin.graves.util;
 
+import me.mgin.graves.Graves;
 import net.minecraft.util.Identifier;
 
 public class Identifiers {
-  static public Identifier CLIENT_SEND_CONFIG = new Identifier("CLIENT.SEND.CONFIG");
+  static public Identifier CLIENT_SEND_CONFIG = new Identifier(Graves.MOD_ID, "client_send_config");
 }

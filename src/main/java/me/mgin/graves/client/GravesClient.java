c25e2b8267ee258bde329f592737a0a01c15fcbd
ginsm <mattg.0609@gmail.com>
2022-02-03 05:47:02 -0800

    Remove client-side command initialization

--- src/main/java/me/mgin/graves/client/GravesClient.java
+++ src/main/java/me/mgin/graves/client/GravesClient.java
@@ -1,10 +1,8 @@
 package me.mgin.graves.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 
-import me.mgin.graves.client.commands.ReloadClientCommand;
 import me.mgin.graves.client.registry.ClientEvents;
 import me.mgin.graves.client.render.GraveBlockEntityRenderer;
 import me.mgin.graves.registry.GraveBlocks;
@@ -13,9 +11,6 @@ public class GravesClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		BlockEntityRendererRegistry.register(GraveBlocks.GRAVE_BLOCK_ENTITY, GraveBlockEntityRenderer::new);
-
-		ClientCommandManager.DISPATCHER.register(
-				ClientCommandManager.literal("reloadgraves").executes(context -> ReloadClientCommand.execute(context)));
 
 		ClientEvents.register();
 	}

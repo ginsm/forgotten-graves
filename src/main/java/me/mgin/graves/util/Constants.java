0a955bdd2c93cb9ad51f6983d193c54cbbdbe6c5
ginsm <mattg.0609@gmail.com>
2022-02-03 22:56:10 -0800

    Remove FORGET_CLIENT_CONFIG constant

--- src/main/java/me/mgin/graves/util/Constants.java
+++ src/main/java/me/mgin/graves/util/Constants.java
@@ -5,6 +5,5 @@ import net.minecraft.util.Identifier;
 
 public class Constants {
 	static public Identifier SEND_CLIENT_CONFIG = new Identifier(Graves.MOD_ID, "send_client_config");
-	static public Identifier FORGET_CLIENT_CONFIG = new Identifier(Graves.MOD_ID, "forget_client_config");
 	static public Identifier UPDATE_CLIENTSIDE_CONFIG = new Identifier(Graves.MOD_ID, "update_clientside_config");
 }

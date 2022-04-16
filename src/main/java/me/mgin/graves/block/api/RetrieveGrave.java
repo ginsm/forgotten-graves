c1b1d6cd35910b75a003f13a2172c2bf64bc4593
ginsm <mattg.0609@gmail.com>
2022-04-16 00:27:44 -0700

    Add unloaded inventories to extraItems arr

--- src/main/java/me/mgin/graves/block/api/RetrieveGrave.java
+++ src/main/java/me/mgin/graves/block/api/RetrieveGrave.java
@@ -69,6 +69,12 @@ public class RetrieveGrave {
 				}
 			}
 
+			// Check for any potentionally unloaded inventories; store them if found
+			for (String modID : Graves.unloadedInventories) {
+				DefaultedList<ItemStack> inventory = graveEntity.getInventory(modID);
+				if (inventory != null) extraItems.addAll(inventory);
+			}
+
 			// Preserve previous inventory
 			extraItems.addAll(oldInventory);
 

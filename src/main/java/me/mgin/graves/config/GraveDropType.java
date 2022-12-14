974c233de3b6200e116705b5fcfb58d7a66987e7
Ginsm <mattg.0609@gmail.com>
2022-12-13 21:34:25 -0500

    Simplify option type information (i.e. STORE_ALL_XP -> ALL)

--- src/main/java/me/mgin/graves/config/GraveDropType.java
+++ src/main/java/me/mgin/graves/config/GraveDropType.java
@@ -1,5 +1,5 @@
 package me.mgin.graves.config;
 
 public enum GraveDropType {
-	DROP_ITEMS, PUT_IN_INVENTORY
+	DROP, INVENTORY
 }
--- src/main/java/me/mgin/graves/config/GraveRetrievalType.java
+++ src/main/java/me/mgin/graves/config/GraveRetrievalType.java
@@ -1,5 +1,5 @@
 package me.mgin.graves.config;
 
 public enum GraveRetrievalType {
-	ON_USE, ON_BREAK, ON_BOTH
+	USE, BREAK, BOTH
 }

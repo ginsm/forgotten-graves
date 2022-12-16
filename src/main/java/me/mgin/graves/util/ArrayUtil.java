05a42c0bf865ec9c6eb069757c9c9fdb31c4b59a
Ginsm <mattg.0609@gmail.com>
2022-12-16 10:15:24 -0500

    General code cleanup

--- src/main/java/me/mgin/graves/util/ArrayUtil.java
+++ src/main/java/me/mgin/graves/util/ArrayUtil.java
@@ -1,8 +1,5 @@
 package me.mgin.graves.util;
 
-import java.sql.Array;
-import java.util.ArrayList;
-
 public class ArrayUtil {
     static public <T> int indexOf(T[] array, T target) {
         int index = -1;

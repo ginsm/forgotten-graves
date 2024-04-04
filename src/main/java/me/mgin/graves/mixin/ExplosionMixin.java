c5221b62019e7a6dd08802c83f2d18a05b7caeaa
Ginsm <mattg.0609@gmail.com>
2024-04-03 20:15:49 -0400

    Fix explosions not causing terrain damage

--- src/main/java/me/mgin/graves/mixin/ExplosionMixin.java
+++ src/main/java/me/mgin/graves/mixin/ExplosionMixin.java
@@ -1,15 +1,31 @@
 package me.mgin.graves.mixin;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import me.mgin.graves.block.entity.GraveBlockEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Explosion.class)
 public class ExplosionMixin {
-    // This prevents creepers from blowing up graves as they're spawning.
-    @ModifyVariable(method = "affectWorld", at = @At("STORE"), ordinal = 1, print = false)
-    private boolean modifyAffectedBlocks(boolean bool) {
-        return false;
+    @Shadow @Final
+    private World world;
+
+    @Shadow @Final
+    private ObjectArrayList<BlockPos> affectedBlocks;
+
+    @Inject(method = "affectWorld", at = @At("HEAD"))
+    private void removeGravesFromAffectedBlocks(boolean particles, CallbackInfo ci) {
+        for (BlockPos blockPos : this.affectedBlocks) {
+            BlockEntity blockEntity = world.getBlockEntity(blockPos);
+            if (blockEntity instanceof GraveBlockEntity) affectedBlocks.remove(blockPos);
+        }
     }
 }

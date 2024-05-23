a78fc5e0ab5bf37c1348887812a730b869d6701c
Ginsm <mattg.0609@gmail.com>
2024-05-23 01:51:43 -0400

    (#93) Create abstraction classes

---
+++ src/main/java/me/mgin/graves/abstraction/FabricAbstraction.java
@@ -0,0 +1,11 @@
+package me.mgin.graves.abstraction;
+
+/**
+ * This class contains method abstractions for Fabric. This is to be used in conjunction
+ * with stonecutter-kt to keep the rest of the codebase version agnostic.
+ *
+ * @see <a href="https://github.com/kikugie/stonecutter-kt">Stonecutter KT</a>
+ */
+public class FabricAbstraction {
+
+}

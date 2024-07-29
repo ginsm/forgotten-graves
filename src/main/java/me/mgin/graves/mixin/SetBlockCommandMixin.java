c03bc979bd9846ea0f3c61a4deebef383a3db5ec
Ginsm <mattg.0609@gmail.com>
2024-07-29 16:27:43 -0400

    Allow setblock to replace player-owned graves

---
+++ src/main/java/me/mgin/graves/mixin/SetBlockCommandMixin.java
@@ -0,0 +1,37 @@
+package me.mgin.graves.mixin;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import me.mgin.graves.block.GraveBlockBase;
+import me.mgin.graves.block.entity.GraveBlockEntity;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.pattern.CachedBlockPosition;
+import net.minecraft.command.argument.BlockStateArgument;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.command.SetBlockCommand;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Predicate;
+
+@Mixin(SetBlockCommand.class)
+public class SetBlockCommandMixin {
+
+    @Inject(method = "execute", at = @At("HEAD"))
+    private static void execute$setGraveBrokenByPlayer(ServerCommandSource source, BlockPos pos,
+                                                       BlockStateArgument block, SetBlockCommand.Mode mode, @Nullable Predicate<CachedBlockPosition> condition, CallbackInfoReturnable<Integer> cir) {
+        ServerWorld world = source.getWorld();
+        Block blockAtPos = world.getBlockState(pos).getBlock();
+
+        // Mark grave block as broken by player so that the command actually succeeds
+        if (blockAtPos instanceof GraveBlockBase graveBlock) {
+            graveBlock.setBrokenByPlayer(true);
+        }
+    }
+}

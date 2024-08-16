c0213ec1a5e3a4a1f395dcffbcb719fa5eef4062
Ginsm <mattg.0609@gmail.com>
2024-08-15 20:00:15 -0400

    Particles might be sorted?
    No idea, really. There's other things wrong with the codebase that I'll
    need to address before I can check.

--- src/main/java/me/mgin/graves/block/utility/Particles.java
+++ src/main/java/me/mgin/graves/block/utility/Particles.java
@@ -2,15 +2,25 @@ package me.mgin.graves.block.utility;
 
 import java.util.Random;
 
-import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+//? if >=1.20.5 {
+import net.minecraft.particle.ParticleEffect;
+//?} else {
+/*import net.minecraft.particle.DefaultParticleType;*/
+//?}
 
 public class Particles {
     private static final Random rng = new Random();
 
-    static public void spawnAtBlock(World world, BlockPos pos, DefaultParticleType type, int amount,
+    //? if >=1.20.5 {
+    static public void spawnAtBlock(World world, BlockPos pos, ParticleEffect type, int amount,
                                     double maxVelocity) {
+    //?} else {
+    /*static public void spawnAtBlock(World world, BlockPos pos, DefaultParticleType type, int amount,
+                                    double maxVelocity) {*/
+    //?}
         for (int i = 0; i <= amount; i++) {
             double blockX = pos.getX() + rng.nextDouble();
             double blockY = pos.getY() + rng.nextDouble();
@@ -26,16 +36,14 @@ public class Particles {
 
     /**
      * Spawn particles at the bottom of a block
-     *
-     * @param world World
-     * @param pos BlockPos
-     * @param type DefaultParticleType
-     * @param amount int
-     * @param maxVelocity double
-     * @param maxStartHeight double
      */
-    static public void spawnAtBlockBottom(World world, BlockPos pos, DefaultParticleType type, int amount,
+    //? if >=1.20.5 {
+    static public void spawnAtBlockBottom(World world, BlockPos pos, ParticleEffect type, int amount,
                                           double maxVelocity, double maxStartHeight) {
+    //?} else {
+    /*static public void spawnAtBlockBottom(World world, BlockPos pos, DefaultParticleType type, int amount,
+                                          double maxVelocity, double maxStartHeight) {*/
+    //?}
         for (int i = 0; i <= amount; i++) {
             double blockX = pos.getX() + rng.nextDouble();
             double blockY = Math.min(pos.getY() + rng.nextDouble(), pos.getY() + maxStartHeight);

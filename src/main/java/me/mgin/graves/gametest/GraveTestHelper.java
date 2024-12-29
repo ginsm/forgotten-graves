bd33df957fde15903b1033b4f5e94d0ee8894391
Ginsm <mattg.0609@gmail.com>
2024-12-29 04:47:31 -0500

    Move helper methods to PlaceGraveTest
    They're pretty specific to PlaceGrave, doubt it'll be re-usable.

--- src/main/java/me/mgin/graves/gametest/GraveTestHelper.java
+++ src/main/java/me/mgin/graves/gametest/GraveTestHelper.java
@@ -1,15 +1,12 @@
 package me.mgin.graves.gametest;
 
 import me.mgin.graves.block.GraveBlockBase;
-import me.mgin.graves.block.utility.PlaceGrave;
-import me.mgin.graves.block.utility.RetrieveGrave;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.test.TestContext;
@@ -25,18 +22,6 @@ public class GraveTestHelper {
         server.getCommandManager().executeWithPrefix(server.getCommandSource(), command);
     }
 
-    /**
-     * Places a grave at the given {@code pos}, checks the {@code endPos} with {@link TestContext#checkBlock}, and
-     * then removes the grave from the world with {@link RetrieveGrave#retrieveWithInteract}.
-     *
-     * @param context TestContext instance
-     * @param player A mock player; use {@link TestContext#createMockSurvivalPlayer}
-     * @param pos An absolute BlockPos; use {@link TestContext#getAbsolutePos} with a relative BlockPos
-     * @param endPos An absolute BlockPos; use {@link TestContext#getAbsolutePos} with a relative BlockPos
-     * @param worldKey RegistryKey for the given world (OVERWORLD, NETHER, END).
-     */
-    public static void placeGraveCheck(TestContext context, PlayerEntity player, BlockPos pos, BlockPos endPos,
-                                       RegistryKey<World> worldKey) {
         System.out.println(">> Running " + Thread.currentThread().getStackTrace()[2].getMethodName() + " <<");
         World world = Objects.requireNonNull(player.getServer()).getWorld(worldKey);
         if (world != null) {
@@ -54,40 +39,6 @@ public class GraveTestHelper {
         }
     }
 
-    /**
-     * Teleports a player to a given location, gives them an item stack, kills them, ensures a grave didn't spawn,
-     * and ensures the item stack is on the ground.
-     *
-     * @param context TestContext instance
-     * @param player A mock player; use {@link TestContext#createMockSurvivalPlayer}
-     * @param pos An absolute BlockPos; use {@link TestContext#getAbsolutePos} with a relative BlockPos
-     * @param worldKey RegistryKey for the given world (OVERWORLD, NETHER, END).
-     */
-    public static void gravesDisabledCheck(TestContext context, PlayerEntity player, BlockPos pos, RegistryKey<World> worldKey) {
-        System.out.println(">> Running " + Thread.currentThread().getStackTrace()[2].getMethodName() + " <<");
-        World world = Objects.requireNonNull(player.getServer()).getWorld(worldKey);
-        if (world != null) {
-            // Give player items to check for when player dies
-            ItemStack stack = Items.DIAMOND_BLOCK.getDefaultStack();
-            stack.setCount(5);
-            player.giveItemStack(stack);
-
-            // Teleport and kill player
-            BlockPos abs = context.getAbsolutePos(pos);
-            teleportPlayer(player, abs);
-            dropMockPlayerInventory(player, world, abs); // mock players don't drop their inventories upon death
-            player.kill();
-
-            // Ensure grave didn't spawn
-            Block block = world.getBlockState(abs).getBlock();
-            String errorMessage = "Expect block not to be a grave block at " + abs + " got " + block.getName()
-                + " in " + worldKey.getValue();
-            context.assertTrue(!(block instanceof GraveBlockBase), errorMessage);
-
-            // Check for the item stack
-            context.expectItemsAt(Items.DIAMOND_BLOCK, pos, 3, 5);
-            context.killAllEntities(ItemEntity.class); // cleans up the diamond blocks
-        }
     }
 
     public static void teleportPlayer(PlayerEntity player, BlockPos pos) {
--- src/main/java/me/mgin/graves/gametest/tests/PlaceGraveTest.java
+++ src/main/java/me/mgin/graves/gametest/tests/PlaceGraveTest.java
@@ -1,92 +1,103 @@
-package me.mgin.graves.gametest;
+package me.mgin.graves.gametest.tests;
 
+import me.mgin.graves.block.GraveBlockBase;
+import me.mgin.graves.block.utility.PlaceGrave;
+import me.mgin.graves.block.utility.RetrieveGrave;
 import me.mgin.graves.effects.GraveEffects;
+import me.mgin.graves.gametest.GraveTestHelper;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.Objects;
 
 public class PlaceGraveTest {
     // no sink tests
     public static void sinkInLava$false(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(10, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(10, 7, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void sinkInWater$false(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(2, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(2, 7, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void sinkThroughBlocks$false(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(6, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(6, 7, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void sinkInAir$false(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(18, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(18, 7, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     // sink tests
     public static void sinkInLava$true(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(10, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(10, 2, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void sinkInWater$true(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(2, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(2, 2, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void sinkThroughBlocks$true(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(6, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(6, 2, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void sinksInAir$true(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(18, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(18, 2, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void replaceBlocks$false(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(19, 2, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(18, 2, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     public static void replaceBlocks$true(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(18, 2, 2));
         context.getWorld().setBlockState(pos, Blocks.TALL_GRASS.getDefaultState());
-        GraveTestHelper.placeGraveCheck(context, player, pos, pos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, pos, World.OVERWORLD);
     }
 
     // blacklist
     public static void respectsBlacklist(TestContext context, PlayerEntity player) {
         BlockPos pos = context.getAbsolutePos(new BlockPos(14, 7, 2));
         BlockPos endPos = context.getAbsolutePos(new BlockPos(14, 9, 2));
-        GraveTestHelper.placeGraveCheck(context, player, pos, endPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, pos, endPos, World.OVERWORLD);
     }
 
     // other dims
     public static void spawnsInNether(TestContext context, PlayerEntity player) {
         BlockPos pos = new BlockPos(45, 63, 102);
-        GraveTestHelper.placeGraveCheck(context, player, pos, pos, World.NETHER);
+        checkPlaceGrave(context, player, pos, pos, World.NETHER);
     }
 
     public static void spawnsInEnd(TestContext context, PlayerEntity player) {
         BlockPos pos = new BlockPos(57, 56, 88);
-        GraveTestHelper.placeGraveCheck(context, player, pos, pos, World.END);
+        checkPlaceGrave(context, player, pos, pos, World.END);
     }
 
     // Runs min/max boundary enforcement for two dimensions with different boundaries
@@ -94,26 +105,28 @@ public class PlaceGraveTest {
         BlockPos minYOverworldPos = context.getAbsolutePos(new BlockPos(18, -10, 2)); // min is -64
         BlockPos maxYOverworldPos = context.getAbsolutePos(new BlockPos(18, 400, 2)); // max is 319
         BlockPos overworldEndPos = context.getAbsolutePos(new BlockPos(18, 2, 2)); // sinks down for max, rises up for min
-        GraveTestHelper.placeGraveCheck(context, player, minYOverworldPos, overworldEndPos, World.OVERWORLD);
-        GraveTestHelper.placeGraveCheck(context, player, maxYOverworldPos, overworldEndPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, minYOverworldPos, overworldEndPos, World.OVERWORLD);
+        checkPlaceGrave(context, player, maxYOverworldPos, overworldEndPos, World.OVERWORLD);
 
         BlockPos minYEndPos = new BlockPos(100, -200, 100); // min is 0
         BlockPos maxYEndPos = new BlockPos(100, 320, 100); // max is 255
         BlockPos endFinalPos = new BlockPos(100, 1, 100); // sinks down for max, rises up for min
-        GraveTestHelper.placeGraveCheck(context, player, minYEndPos, endFinalPos, World.END);
-        GraveTestHelper.placeGraveCheck(context, player, maxYEndPos, endFinalPos, World.END);
+        checkPlaceGrave(context, player, minYEndPos, endFinalPos, World.END);
+        checkPlaceGrave(context, player, maxYEndPos, endFinalPos, World.END);
     }
 
     // grave shouldn't spawn
+    // NOTE - I don't think these tests really work. The mock player's death doesn't trigger a grave
+    //        spawning anyway.
     public static void graves$false(TestContext context, PlayerEntity player) {
         BlockPos pos = new BlockPos(18, 2, 2);
-        GraveTestHelper.gravesDisabledCheck(context, player, pos, World.OVERWORLD);
+        checkGravesDisabled(context, player, pos, World.OVERWORLD);
     }
 
     public static void respectsDisableEffect(TestContext context, PlayerEntity player) {
         BlockPos pos = new BlockPos(18, 2, 2);
         player.addStatusEffect(new StatusEffectInstance(GraveEffects.DISABLE_GRAVES_EFFECT, 300));
-        GraveTestHelper.gravesDisabledCheck(context, player, pos, World.OVERWORLD);
+        checkGravesDisabled(context, player, pos, World.OVERWORLD);
     }
 
     public static void disableInPvP$true(TestContext context, PlayerEntity player) {
@@ -122,6 +135,74 @@ public class PlaceGraveTest {
         player.setPosition(0, -58, 0);
         player2.setPosition(0, -58, 0);
         player2.attack(player);
-        GraveTestHelper.gravesDisabledCheck(context, player, pos, World.OVERWORLD);
+        checkGravesDisabled(context, player, pos, World.OVERWORLD);
+    }
+
+    // NOTE - Class-specific Helper Functions
+    /**
+     * Places a grave at the given {@code pos}, checks the {@code endPos} with {@link TestContext#checkBlock}, and
+     * then removes the grave from the world with {@link RetrieveGrave#retrieveWithInteract}.
+     *
+     * @param context TestContext instance
+     * @param player A mock player; use {@link TestContext#createMockSurvivalPlayer}
+     * @param pos An absolute BlockPos; use {@link TestContext#getAbsolutePos} with a relative BlockPos
+     * @param endPos An absolute BlockPos; use {@link TestContext#getAbsolutePos} with a relative BlockPos
+     * @param worldKey RegistryKey for the given world (OVERWORLD, NETHER, END).
+     */
+    public static void checkPlaceGrave(TestContext context, PlayerEntity player, BlockPos pos, BlockPos endPos,
+                                       RegistryKey<World> worldKey) {
+        System.out.println(">> Running " + Thread.currentThread().getStackTrace()[2].getMethodName() + " <<");
+        World world = Objects.requireNonNull(player.getServer()).getWorld(worldKey);
+        if (world != null) {
+            // Place the grave
+            PlaceGrave.place(world, GraveTestHelper.posToVec3d(pos), player);
+
+            // Ensure grave placed
+            Block block = world.getBlockState(endPos).getBlock();
+            String errorMessage = "Expect block to be a grave block at " + endPos + " got " + block.getName()
+                + " in " + worldKey.getValue();
+            context.assertTrue(block instanceof GraveBlockBase, errorMessage);
+
+            // Remove grave
+            RetrieveGrave.retrieveWithInteract(player, world, endPos);
+        }
+    }
+
+    /**
+     * Teleports a player to a given location, gives them an item stack, kills them, ensures a grave didn't spawn,
+     * and ensures the item stack is on the ground.
+     *
+     * @param context TestContext instance
+     * @param player A mock player; use {@link TestContext#createMockSurvivalPlayer}
+     * @param pos An absolute BlockPos; use {@link TestContext#getAbsolutePos} with a relative BlockPos
+     * @param worldKey RegistryKey for the given world (OVERWORLD, NETHER, END).
+     */
+    public static void checkGravesDisabled(TestContext context, PlayerEntity player, BlockPos pos, RegistryKey<World> worldKey) {
+        System.out.println(">> Running " + Thread.currentThread().getStackTrace()[2].getMethodName() + " <<");
+        World world = Objects.requireNonNull(player.getServer()).getWorld(worldKey);
+        if (world != null) {
+            // Give player items to check for when player dies
+            ItemStack stack = Items.DIAMOND_BLOCK.getDefaultStack();
+            stack.setCount(5);
+            player.giveItemStack(stack);
+
+            // Teleport and kill player
+            BlockPos abs = context.getAbsolutePos(pos);
+            GraveTestHelper.teleportPlayer(player, abs);
+            GraveTestHelper.dropMockPlayerInventory(player, world, abs); // mock players don't drop their inventories
+            // upon
+            // death
+            player.kill();
+
+            // Ensure grave didn't spawn
+            Block block = world.getBlockState(abs).getBlock();
+            String errorMessage = "Expect block not to be a grave block at " + abs + " got " + block.getName()
+                + " in " + worldKey.getValue();
+            context.assertTrue(!(block instanceof GraveBlockBase), errorMessage);
+
+            // Check for the item stack
+            context.expectItemsAt(Items.DIAMOND_BLOCK, pos, 3, 5);
+            context.killAllEntities(ItemEntity.class); // cleans up the diamond blocks
+        }
     }
 }
